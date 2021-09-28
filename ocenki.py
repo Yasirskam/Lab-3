20cbd55ec04f726a03979eddd269bb4684fcd934
Yasirskam <90673913+Yasirskam@users.noreply.github.com>
2021-09-28 09:06:13 +0300

    /

---
+++ ocenki.py
@@ -0,0 +1,14 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> score  =  int ( input ( "Введите вашу оценку:" ))
+
+если  оценка  > =  90 :
+    print ( "Отлично! Ваша оценка А" )
+ рейтинг  elif > =  80 :
+    print ( "Здорово! Ваша оценка - B" )
+ рейтинг  elif > =  70 :
+    print ( "Хорошо! Ваша оценка - C" )
+ рейтинг  elif > =  60 :
+    print ( "Ваша оценка - Д. Стоит повторить материал." )
+еще :
+    print ( "Вы не сдали экзамен" )
